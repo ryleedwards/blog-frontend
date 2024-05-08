0bf2a201a7b7d6914d4fb491724c918072fbd10b
Ryle Edwards <78249747+ryleedwards@users.noreply.github.com>
2024-05-08 10:56:11 -0500

    Create router with root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
